a4acb6ae6f6ac378c345e9cbc7dbf3ff2e0c0425
root <root@3987771-fg19048>
2024-12-26 09:31:45 +0300

    added time

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app.settings.settings import settings
 import os
 import telebot
-
+import datetime
 # Замените на ваш токен и chat_id
 TELEGRAM_TOKEN = settings.TELEGRAM_TOKEN
 CHAT_ID = settings.CHAT_ID
@@ -16,7 +16,7 @@ def process_log_file(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
         if (len(lines) % 1 == 0):
-            send_message_to_telegram(f"В проекте ERP произошла 500 ошибка!\n Лог: ```\n{lines[-1]}```")
+            send_message_to_telegram(f"В проекте ERP произошла 500 ошибка!\n Лог:\n{lines[-1]}\nДата и время: {datetime.datetime.now()}")
 
 if __name__ == "__main__":
    process_log_file(LOG_FILE_PATH)
